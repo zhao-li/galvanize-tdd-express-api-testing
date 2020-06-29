0558bacab8cd526c23f86e19952b92a7536ad46c
Zhao Li <zhao.li@startingspark.com>
2020-06-29 05:04:50 -1000

    add test structure so tests will run

---
+++ tests/index.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest')
+//const app = require('../index')
+
+const hostUrl = 'https://jsonplaceholder.typicode.com/';
+
+describe('GET /posts/1/comments', () => {
+  it('responds with a 200', (done) => {
+    request(hostUrl)
+      .get('/')
+      //.get('/posts/1/comments')
+      .expect(200, done)
+  })
+})
